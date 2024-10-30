e9aa0bd2c8c7c914c339d54dd09050b341e0bf24
Stas <zergedoo@gmail.com>
2024-10-30 23:06:46 +0500

    .gitignore

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ import { generatePhotos } from './photos.js';
 
 const photoDescriptions = generatePhotos();
 console.log(photoDescriptions);
+
+import { renderThumbnails } from './renderThumbnails.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderThumbnails();
+});
